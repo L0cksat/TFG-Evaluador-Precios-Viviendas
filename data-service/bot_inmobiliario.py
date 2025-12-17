592da1753ea53e4baac1e0e216c084cc612698a8
Nicky Jones De Giorgi <stephennicholas.jones266@comunidadunir.net>
2025-12-17 01:34:59 +0100

    chore: Actualizado el códgio del bot, añadido la lógica del scraping y extracción de datos

--- data-service/bot_inmobiliario.py
+++ data-service/bot_inmobiliario.py
@@ -92,6 +92,94 @@ try:
     driver.save_screenshot("Captura_exito.png")
     print("Captura guardada como 'Captura_exito.png'")
 
+    # 5. ---EXTRACCIÓN DE DATOS---
+    print("Esperando que se carguen las tarjetas de propiedades...")
+    wait.until(EC.presence_of_element_located((By.CLASS_NAME, "listing__container")))
+
+    listas_casas = driver.find_elements(By.CLASS_NAME, "listing__container")
+    print(f"He detectado {len(listas_casas)} casas en total. Seleccionando las más relevantes...")
+
+    datos_comparables = [] # Aquí se guardaran los datos limpios.
+    objetivo_casas = 10 # El limite de casa a un máximo de 10
+
+    for indice, casa in enumerate(listas_casas):
+        # Se va a parar el bucle en cuanto tengamos las propiedades que necesitamos.
+        if len(datos_comparables) >= objetivo_casas:
+            print("Objetivo cumplido. Paramos la búsqueda ^_^")
+            break
+        print(f"---Analizando Casa #{indice + 1}---")
+
+        try:
+            item = {} #Se crea un Diccionario temporal
+
+            # 5.1 Extracción del precio de la vivienda
+            try:
+                precio_texto = casa.find_element(By.CSS_SELECTOR, ".price h4").text
+                item['precio_raw'] = precio_texto.replace(".", "").replace("€", "").strip() # Limpiamos el dato para luego realizar la futura calculación. Se convierte 875.000 € -> 875000
+            except:
+                item['precio_raw'] = "0" # En este caso si no tiene precio, no nos sirve para el calculo.
+            
+            # Si el precio de la casa es 0 o "Consultar", se salta directamente.
+            if not item["precio_raw"].isdigit():
+                print("Casa descartada: Sin Precio Válido")
+                continue
+
+            # 5.2 Extracción de los metros cuadrados
+            try:
+                # Encontramos el primer span que está dentro de details.
+                m2_texto = casa.find_element(By.CSS_SELECTOR, ".card__details span:nth-of-type(1)").text
+                item['m2'] = m2_texto.replace("m²", "").strip() # Limpieza del dato, convertimos "167 m²" -> "167"
+            except:
+                item['m2'] ="0"
+
+            # 5.3 Extracción del número de habitaciones
+            try:
+                # Encontramos los número de habitaciones
+                habs_texto = casa.find_element(By.CSS_SELECTOR, ".card__details span:nth-of-type(2)").text
+                item['habitaciones'] = habs_texto.strip()
+            except:
+                item['habitaciones'] = "0" 
+            
+            # 5.4 Extracción de la ubicación
+            try:
+                #Buscamos dentro de listing__description suele tener el municipio o barrio
+                ubi_texto = casa.find_element(By.CSS_SELECTOR, ".listing__description h3").text
+                item['ubicacion'] = ubi_texto
+            except:
+                item['ubicacion'] = "Desconocida"
+
+            # 5.5 Extracción del titulo de la tarjeta
+            try:
+                # Buscamos el titulo tipo h2
+                item['titulo'] = casa.find_element(By.TAG_NAME, "h2").text
+            except:
+                item['titulo'] = "Sin titulo"
+
+            # Guardamos el item limpio
+            datos_comparables.append(item)
+            print(f"Guardada: {item['precio_raw']}€ | {item['m2']}m² | {item['habitaciones']}hab | {item['ubicacion']}")
+        except Exception as e:
+            print(f" Error leyendo tarjeta: {e}")
+
+    # 6. Resultado final
+    print("-" * 50)
+    print(f" Scraping completado! ^_^")
+    print(f"Se han extraído {len(datos_comparables)} propiedades para el algoritmo.")
+    print(datos_comparables) # Esto es lo que luego exportaremos al Excel o a otra parte de la app para el calcúlo.
+
+    # 7. Exportación de los datos al formato JSON
+    import json
+
+    nombre_archivo = "resultados_scraping.json"
+
+    with open(nombre_archivo, 'w', encoding='utf-8') as f:
+        # dump volca la lista al archivo
+        # ensure_acii=False permite que se lean las tildes
+        # indent=4 lo deja bonito y legible
+        json.dump(datos_comparables, f, ensure_ascii=False, indent=4)
+
+    print(f"Datos guardados exitosamente en '{nombre_archivo}'")
+
 except Exception as e:
     print("Hubo un error en la búsqueda:", e)
     # Con este bloque nuestro robot hará una captura de pantalla en el momento que falla:
@@ -101,4 +189,4 @@ except Exception as e:
 finally:
     # 4 Cierre del navegador.
     driver.quit()
-    print("---Fin del script---)")
+    print("---Fin del script---")
